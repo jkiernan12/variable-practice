24120154c8d7b1c318592dc925c28fc24af93938
John Kiernan <jkiernan12@yahoo.com>
2021-07-15 22:38:54 -0400

    Add Object data types

--- model-l.js
+++ model-l.js
@@ -9,3 +9,14 @@ var tired = true;
 
 var hobbies = ["homebrewing", "rock climbing"];
 var friends = ["Adam", "Esteban", "Noah"];
+
+var desk = {
+  messy: true,
+  screens: 3,
+  material: "wood"
+}
+var apartment = {
+  isWarm: true,
+  monthlyCost: 950,
+  furniture: ["couch", "desk", "bed"]
+}
